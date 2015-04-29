c2f04c93e81074ca1ae614dace2623fbb37eee21
bogdan.tsap <bogdan.tsap@thinktank.net>
2015-04-29 14:05:28 +0300

    init commit

---
+++ app/controllers/Application.java
@@ -0,0 +1,37 @@
+package controllers;
+
+import play.*;
+import play.libs.MimeTypes;
+import play.mvc.*;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.*;
+
+import models.*;
+
+public class Application extends Controller {
+
+    public static void index() {
+        render();
+    }
+
+    public static void downloadFile(final Long fileId) throws IOException {
+        response.setHeader("Accept-Ranges", "bytes");
+//        notFoundIfNull(fileId);
+
+        String fileName = "Bcee_Think_Twice.mp3";//name of the file
+        File underlyingFile = new File("/home/bohdaq/git/player/public/audios/Bcee_Think_Twice.mp3"); //load file
+
+        Http.Header rangeHeader = request.headers.get("range");
+        if (rangeHeader != null) {
+            throw new PartialContent(underlyingFile, fileName);
+        } else {
+            renderBinary(new FileInputStream(underlyingFile),
+                    fileName, underlyingFile.length(),
+                    MimeTypes.getContentType(fileName), false);
+        }
+    }
+
+}
