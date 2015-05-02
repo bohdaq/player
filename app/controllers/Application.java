47bfa2c5b15be4aff1310504253b43fe4d8c4be9
bogdan.tsap <bogdan.tsap@thinktank.net>
2015-05-02 18:36:04 +0300

    waveform + mp3 idv tags

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,14 @@
 package controllers;
 
+import com.mpatric.mp3agic.ID3v1;
+import com.mpatric.mp3agic.ID3v2;
+import com.mpatric.mp3agic.Mp3File;
 import models.Audio;
 import play.*;
 import play.libs.MimeTypes;
 import play.mvc.*;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.util.List;
 
 import play.data.*;
@@ -29,6 +29,56 @@ public class Application extends Controller {
         FileOutputStream out = new FileOutputStream(HOMEROUTE + "/public/audios/" + filename);
         out.write(audioFile.asBytes());
         out.close();
+
+        Mp3File mp3file = new Mp3File(HOMEROUTE + "/public/audios/" + filename);
+        System.out.println("Length of this mp3 is: " + mp3file.getLengthInSeconds() + " seconds");
+        System.out.println("Bitrate: " + mp3file.getLengthInSeconds() + " kbps " + (mp3file.isVbr() ? "(VBR)" : "(CBR)"));
+        System.out.println("Sample rate: " + mp3file.getSampleRate() + " Hz");
+        System.out.println("Has ID3v1 tag?: " + (mp3file.hasId3v1Tag() ? "YES" : "NO"));
+        System.out.println("Has ID3v2 tag?: " + (mp3file.hasId3v2Tag() ? "YES" : "NO"));
+        System.out.println("Has custom tag?: " + (mp3file.hasCustomTag() ? "YES" : "NO"));
+
+        if (mp3file.hasId3v1Tag()) {
+            ID3v1 id3v1Tag = mp3file.getId3v1Tag();
+            System.out.println("Track: " + id3v1Tag.getTrack());
+            System.out.println("Artist: " + id3v1Tag.getArtist());
+            System.out.println("Title: " + id3v1Tag.getTitle());
+            System.out.println("Album: " + id3v1Tag.getAlbum());
+            System.out.println("Year: " + id3v1Tag.getYear());
+            System.out.println("Genre: " + id3v1Tag.getGenre() + " (" + id3v1Tag.getGenreDescription() + ")");
+            System.out.println("Comment: " + id3v1Tag.getComment());
+        }
+        if (mp3file.hasId3v2Tag()) {
+            ID3v2 id3v2Tag = mp3file.getId3v2Tag();
+            System.out.println("Track: " + id3v2Tag.getTrack());
+            System.out.println("Artist: " + id3v2Tag.getArtist());
+            System.out.println("Title: " + id3v2Tag.getTitle());
+            System.out.println("Album: " + id3v2Tag.getAlbum());
+            System.out.println("Year: " + id3v2Tag.getYear());
+            System.out.println("Genre: " + id3v2Tag.getGenre() + " (" + id3v2Tag.getGenreDescription() + ")");
+            System.out.println("Comment: " + id3v2Tag.getComment());
+            System.out.println("Composer: " + id3v2Tag.getComposer());
+            System.out.println("Publisher: " + id3v2Tag.getPublisher());
+            System.out.println("Original artist: " + id3v2Tag.getOriginalArtist());
+            System.out.println("Album artist: " + id3v2Tag.getAlbumArtist());
+            System.out.println("Copyright: " + id3v2Tag.getCopyright());
+            System.out.println("URL: " + id3v2Tag.getUrl());
+            System.out.println("Encoder: " + id3v2Tag.getEncoder());
+        }
+
+        if (mp3file.hasId3v2Tag()) {
+            ID3v2 id3v2Tag = mp3file.getId3v2Tag();
+            byte[] imageData = id3v2Tag.getAlbumImage();
+            if (imageData != null) {
+                String mimeType = id3v2Tag.getAlbumImageMimeType();
+                System.out.println("Mime type: " + mimeType);
+                // Write image to file - can determine appropriate file extension from the mime type
+                RandomAccessFile file = new RandomAccessFile(HOMEROUTE + "/public/album-artwork/" + filename+".artwork", "rw");
+                file.write(imageData);
+                file.close();
+            }
+        }
+
         index();
     }
 
--- push.sh
+++ push.sh
@@ -1,2 +1,2 @@
 git push origin master
-ssh hamelemon 'cd ~/; source .bash_profile; cd /home/bogdan/player; git stash; git pull; git stash apply; play restart;'
+ssh hamelemon 'cd ~/; source .bash_profile; cd /home/bogdan/player; git stash; git pull; git stash apply;play deps;play restart;'
