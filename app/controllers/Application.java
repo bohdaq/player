31cdf421d7d892bea783875597c54812bf984762
bogdan.tsap <bogdan.tsap@thinktank.net>
2015-05-12 19:57:34 +0300

    password missmatch check

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -39,12 +39,15 @@ public class Application extends Controller {
     }
 
     public static void register(String email, String password, String xpassword){
-        System.out.println(email + password + xpassword);
-        User user = new User(email, password);
-        user.token = System.currentTimeMillis() + email;
-        user.save();
-        response.setCookie("token", user.token);
-        renderJSON("{ \"status\": \"User registered\", \"token\": \"" + user.token + "\"}");
+        if(password.equals(xpassword)){
+            User user = new User(email, password);
+            user.token = System.currentTimeMillis() + email;
+            user.save();
+            response.setCookie("token", user.token);
+            renderJSON("{ \"status\": \"User registered\", \"token\": \"" + user.token + "\"}");
+        }else{
+            renderJSON("{ \"status\": \"Password missmatch\"}");
+        }
     }
 
     public static void index() {
@@ -70,48 +73,41 @@ public class Application extends Controller {
         String filename = audioFile.getFileName();
         Audio audioEntity = new Audio(filename);
         audioEntity.user = user;
-        audioEntity.save();
 
         FileOutputStream out = new FileOutputStream(HOMEROUTE + "/public/audios/" + filename);
         out.write(audioFile.asBytes());
         out.close();
 
         Mp3File mp3file = new Mp3File(HOMEROUTE + "/public/audios/" + filename);
-        System.out.println("Length of this mp3 is: " + mp3file.getLengthInSeconds() + " seconds");
-        System.out.println("Bitrate: " + mp3file.getLengthInSeconds() + " kbps " + (mp3file.isVbr() ? "(VBR)" : "(CBR)"));
-        System.out.println("Sample rate: " + mp3file.getSampleRate() + " Hz");
-        System.out.println("Has ID3v1 tag?: " + (mp3file.hasId3v1Tag() ? "YES" : "NO"));
-        System.out.println("Has ID3v2 tag?: " + (mp3file.hasId3v2Tag() ? "YES" : "NO"));
-        System.out.println("Has custom tag?: " + (mp3file.hasCustomTag() ? "YES" : "NO"));
+        audioEntity.length = mp3file.getLengthInSeconds();
+        audioEntity.bitrate = mp3file.getBitrate();
+        audioEntity.sampleRate = mp3file.getSampleRate();
+        audioEntity.hasId3v1Tag = mp3file.hasId3v1Tag();
+        audioEntity.hasId3v2Tag = mp3file.hasId3v2Tag();
+        audioEntity.hasCustomTag = mp3file.hasCustomTag();
 
         if (mp3file.hasId3v1Tag()) {
             ID3v1 id3v1Tag = mp3file.getId3v1Tag();
-            System.out.println("Track: " + id3v1Tag.getTrack());
-            System.out.println("Artist: " + id3v1Tag.getArtist());
-            System.out.println("Title: " + id3v1Tag.getTitle());
-            System.out.println("Album: " + id3v1Tag.getAlbum());
-            System.out.println("Year: " + id3v1Tag.getYear());
-            System.out.println("Genre: " + id3v1Tag.getGenre() + " (" + id3v1Tag.getGenreDescription() + ")");
-            System.out.println("Comment: " + id3v1Tag.getComment());
+            System.out.println(id3v1Tag.getArtist());
+            audioEntity.artist = id3v1Tag.getArtist();
+            audioEntity.title = id3v1Tag.getTitle();
+            audioEntity.album = id3v1Tag.getAlbum();
+            audioEntity.year = id3v1Tag.getYear();
+            audioEntity.genre = id3v1Tag.getGenre();
+            audioEntity.genreDescription = id3v1Tag.getGenreDescription();
         }
         if (mp3file.hasId3v2Tag()) {
             ID3v2 id3v2Tag = mp3file.getId3v2Tag();
-            System.out.println("Track: " + id3v2Tag.getTrack());
-            System.out.println("Artist: " + id3v2Tag.getArtist());
-            System.out.println("Title: " + id3v2Tag.getTitle());
-            System.out.println("Album: " + id3v2Tag.getAlbum());
-            System.out.println("Year: " + id3v2Tag.getYear());
-            System.out.println("Genre: " + id3v2Tag.getGenre() + " (" + id3v2Tag.getGenreDescription() + ")");
-            System.out.println("Comment: " + id3v2Tag.getComment());
-            System.out.println("Composer: " + id3v2Tag.getComposer());
-            System.out.println("Publisher: " + id3v2Tag.getPublisher());
-            System.out.println("Original artist: " + id3v2Tag.getOriginalArtist());
-            System.out.println("Album artist: " + id3v2Tag.getAlbumArtist());
-            System.out.println("Copyright: " + id3v2Tag.getCopyright());
-            System.out.println("URL: " + id3v2Tag.getUrl());
-            System.out.println("Encoder: " + id3v2Tag.getEncoder());
+            System.out.println(id3v2Tag.getArtist());
+            audioEntity.artist = id3v2Tag.getArtist();
+            audioEntity.title = id3v2Tag.getTitle();
+            audioEntity.album = id3v2Tag.getAlbum();
+            audioEntity.year = id3v2Tag.getYear();
+            audioEntity.genre = id3v2Tag.getGenre();
+            audioEntity.genreDescription = id3v2Tag.getGenreDescription();
+            audioEntity.encoder = id3v2Tag.getEncoder();
         }
-
+        audioEntity.save();
         if (mp3file.hasId3v2Tag()) {
             ID3v2 id3v2Tag = mp3file.getId3v2Tag();
             byte[] imageData = id3v2Tag.getAlbumImage();
@@ -119,12 +115,12 @@ public class Application extends Controller {
                 String mimeType = id3v2Tag.getAlbumImageMimeType();
                 System.out.println("Mime type: " + mimeType);
                 // Write image to file - can determine appropriate file extension from the mime type
-                RandomAccessFile file = new RandomAccessFile(HOMEROUTE + "/public/album-artwork/" + filename+".artwork", "rw");
+                RandomAccessFile file = new RandomAccessFile(HOMEROUTE + "/public/album-artwork/" + audioEntity.getId()+".jpg", "rw");
                 file.write(imageData);
                 file.close();
             }
         }
-
+        audioEntity.save();
         index();
     }
 
--- app/models/Audio.java
+++ app/models/Audio.java
@@ -7,11 +7,25 @@ import javax.persistence.ManyToOne;
 
 @Entity
 public class Audio extends Model {
+    public String year;
+    public String name;
+    public String artist;
+    public String title;
+    public String album;
+    public Long length;
+    public Integer bitrate;
+    public Integer sampleRate;
+    public Integer genre;
+    public String genreDescription;
+    public String encoder;
+    public boolean hasId3v1Tag;
+    public boolean hasId3v2Tag;
+    public boolean hasCustomTag;
+
+
     public Audio(String name){
         this.name = name;
     }
-
-    public String name;
 
     @ManyToOne
     public User user;
