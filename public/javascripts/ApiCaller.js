de6831d0ff1353a76fbee2f237f8b4659eaf25df
bogdan.tsap <bogdan.tsap@thinktank.net>
2015-05-19 20:37:00 +0300

    ApiCaller usage on all templates

--- public/javascripts/ApiCaller.js
+++ public/javascripts/ApiCaller.js
@@ -1,6 +1,15 @@
-var ApiCaller = function() {
+var ApiCaller = function() {};
 
-};
 ApiCaller.prototype.deleteAudio = function(url, doneCallback) {
     $.post(url).done(doneCallback);
 };
+
+ApiCaller.prototype.registerUser = function(url, data, doneCallback) {
+    $.post(url, data).done(doneCallback);
+};
+
+ApiCaller.prototype.loginUser = function(url, data, doneCallback) {
+    $.post(url, data).done(doneCallback);
+};
+
+window.apiCaller = new ApiCaller();
